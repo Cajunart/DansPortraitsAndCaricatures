f593e9afb695d4f62eb98101cbcd198b24275c1c
Cajunsite <fosterba@mail.com>
2016-02-27 15:27:46 -0500

    We're adding all the files for the first time.

---
+++ js/render-template.js
@@ -0,0 +1,13 @@
+﻿function render(e, t) {
+    if (!render.tmpl_cache) {
+        render.tmpl_cache = {}
+    }
+    if (!render.tmpl_cache[e]) {
+        var n = "templates";
+        var r = n + "/" + e + ".html";
+        var i;
+        $.ajax({ url: r, method: "GET", async: false, success: function (e) { i = e } });
+        render.tmpl_cache[e] = Handlebars.compile(i)
+    }
+    return render.tmpl_cache[e](t) 
+}
